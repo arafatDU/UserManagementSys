0edebc4c7f3593bc970b4a454e20282a57d2ea08
arafatDU <bsse1435@iit.du.ac.bd>
2025-05-04 02:52:35 +0600

    Spring boot project initialized

---
+++ src/main/java/com/arafat/UserManagementSys/UserManagementSysApplication.java
@@ -0,0 +1,13 @@
+package com.arafat.UserManagementSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementSysApplication.class, args);
+	}
+
+}
